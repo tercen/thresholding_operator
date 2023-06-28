34487e159322a06a1d2e0c3732e046813fe90a86
agouy <contact@agouy.com>
2023-06-28 06:53:04 +0000

    Refactoring for performance

--- main.R
+++ main.R
@@ -1,54 +1,35 @@
 library(tercen)
 library(dplyr)
+library(data.table)
 
-do.flag = function(df, operand = ">=", pass.flag = "pass", fail.flag = "fail") {
+do.flag <- function(df, operand = ">=") {
   
-  values <- df %>% group_by(.axisIndex) %>% 
-    summarise(value = mean(.y, na.rm = TRUE)) %>% 
-    filter(.axisIndex == 0)
-  val <- values$value
+  val <- df[.axisIndex == 0]$.y
+  if(length(val) != 1) val <- NaN
   
-  if(length(val) == 0) val <- NaN
+  thres <- df[.axisIndex == 1]$.y
+  if(length(thres) != 1) thres <- NaN
   
-  thresholds <- df %>% group_by(.axisIndex) %>% 
-    summarise(value = mean(.y, na.rm = TRUE)) %>% 
-    filter(.axisIndex == 1)
-  thres <- thresholds$value
-  
-  if(length(thres) == 0) thres <- NaN
-  
-  flag <- ifelse(
-    eval(parse(text = paste0("val ", operand, " thres"))),
-    pass.flag,
-    fail.flag
-  )
-
-  return(data.frame(
-    .ri = df$.ri[1],
-    .ci = df$.ci[1],
-    flag = flag
-  ))
+  flag <- do.call(operand, list(val, thres))
+  return(list(flag = flag))
 }
 
 ctx = tercenCtx()
 
-if (nrow(unique(ctx$select(c('.axisIndex')))) != 2)
-  stop("Two layers are required.")
+if(length(ctx$yAxis) != 2) stop("Two layers are required.")
 
-operand <- ifelse(
-  !is.null(ctx$op.value('operand')),
-  as.character(ctx$op.value('operand')),
-  ">="
-)
+operand <- ctx$op.value('operand', as.character, ">=")
+pass.flag <- ctx$op.value("pass.flag", as.character, "pass")
+fail.flag <- ctx$op.value("fail.flag", as.character, "fail")
 
-pass.flag <- "pass"
-if(!is.null(ctx$op.value("pass.flag"))) pass.flag <- ctx$op.value("pass.flag")
-fail.flag <- "fail"
-if(!is.null(ctx$op.value("fail.flag"))) fail.flag <- ctx$op.value("fail.flag")
-
-df_out <- ctx %>%
+df <- ctx %>%
   select(.ci, .ri, .y, .axisIndex) %>%
-  group_by(.ci, .ri) %>%
-  do(do.flag(., operand, pass.flag, fail.flag)) %>%
+  as.data.table()
+
+df_out <- df[, do.flag(.SD, operand), by = .(ci = .ci, ri = .ri)]
+
+df_out %>%
+  as_tibble() %>%
+  mutate(flag = if_else(flag, pass.flag, fail.flag)) %>%
   ctx$addNamespace() %>%
   ctx$save()
